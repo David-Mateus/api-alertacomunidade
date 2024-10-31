2feb6bb7d48eece639735ee693a228f9e313d434
David-Mateus <david-mateus09@hotmail.com>
2024-10-31 09:23:59 -0300

    refact: refatorando o repostorio

--- src/main/java/tech/davidmateus/alertacomunidade/repository/RoleRepository.java
+++ src/main/java/tech/davidmateus/alertacomunidade/repository/RoleRepository.java
@@ -2,7 +2,8 @@ package tech.davidmateus.alertacomunidade.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import tech.davidmateus.alertacomunidade.entities.Role;
 
 @Repository
-public interface Role extends JpaRepository<Role, Long> {
+public interface RoleRepository extends JpaRepository<Role, Long> {
 }
--- src/main/java/tech/davidmateus/alertacomunidade/repository/TweetRepository.java
+++ src/main/java/tech/davidmateus/alertacomunidade/repository/TweetRepository.java
@@ -2,7 +2,8 @@ package tech.davidmateus.alertacomunidade.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import tech.davidmateus.alertacomunidade.entities.Tweet;
 
 @Repository
-public interface Tweet extends JpaRepository<Tweet, Long> {
+public interface TweetRepository extends JpaRepository<Tweet, Long> {
 }
