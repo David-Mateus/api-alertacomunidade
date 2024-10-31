3a1d997315fc373a4887858d38c1b0a133af936f
David-Mateus <david-mateus09@hotmail.com>
2024-10-31 09:54:28 -0300

    feat: gerando chaves publicas e privadas

--- src/main/java/tech/davidmateus/alertacomunidade/config/SecurityConfig.java
+++ src/main/java/tech/davidmateus/alertacomunidade/config/SecurityConfig.java
@@ -1,18 +1,33 @@
 package tech.davidmateus.alertacomunidade.config;
 // configuração o spring security
 
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
+    @Value("${jwt.public.key}")
+    private RSAPublicKey publicKey;
+    @Value("${jwt.private.key}")
+    private RSAPrivateKey privateKey;
     @Bean
     private SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         //customizar toda questão de segurança do projeto
@@ -22,5 +37,17 @@ public class SecurityConfig {
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)); //Spring Security não manterá informações de sessão entre requisições.
         return http.build();
+    }
+    @Bean
+    public JwtDecoder jwtDecoder(){
+        // descriptografa o jwt
+        return NimbusJwtDecoder.withPublicKey(publicKey).build();
+    }
+    @Bean
+    public JwtEncoder jwtEncoder(){
+        // criptografa o jwt
+        JWK jwk = new RSAKey.Builder(this.publicKey).privateKey(privateKey).build();
+        var jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
+        return new NimbusJwtEncoder(jwks);
     }
 }
