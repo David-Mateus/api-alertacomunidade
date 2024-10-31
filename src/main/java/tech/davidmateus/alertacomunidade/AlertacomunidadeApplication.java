11b6ff8dee56526603e2a729000676a2e78162a3
David-Mateus <david-mateus09@hotmail.com>
2024-10-31 08:19:04 -0300

    feat: init commit

---
+++ src/main/java/tech/davidmateus/alertacomunidade/AlertacomunidadeApplication.java
@@ -0,0 +1,13 @@
+package tech.davidmateus.alertacomunidade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlertacomunidadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlertacomunidadeApplication.class, args);
+	}
+
+}
